d1751d678c6805b65af62dfa00fe46fa15f991ef
orayodeji <orayodeji@gmail.com>
2020-10-07 16:33:46 +0100

    ok

---
+++ doublyLinkedList.js
@@ -0,0 +1,104 @@
+/*
+function Node(val){
+    this.val = val;
+    this.prev = null;
+    this.next = null;
+}
+
+function DoublyLinkedList(){
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+}
+*/
+
+class Node{
+    constructor(val){
+        this.val = val;
+        this.prev = null;
+        this.next = null; 
+    }
+}
+
+class DoublyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push(val) {
+        let newNode = new Node(val);
+        if (this.head === null) {
+           this.head = newNode;
+           this.tail = newNode;
+        } else {
+          this.tail.next = newNode;
+          newNode.prev = this.tail
+          this.tail = newNode;
+        }
+        this.length++
+        return this;
+       }
+     
+       pop() {
+        if (!this.head)
+            return undefined;
+        let currentTail = this.tail;
+        if (this.length === 1) {
+            this.head = null
+            this.tail = null
+         } else {
+            this.tail = currentTail.prev;
+            this.tail.next = null;
+            currentTail.prev = null;
+         }
+          this.length--;
+          return currentTail;
+       }
+       shift() {
+        let oldHead = this.head;
+        if (this.length === 0)
+             return undefined;
+        else if (this.length === 1) {
+                 this.head = null;
+                 this.tail = null;
+        } else {
+                 this.head = oldHead.next;
+                 this.head.prev = null;
+                 oldHead.next = null;
+        }
+        this.length--;
+        return oldHead;
+     }
+     unshift(val) {
+        let newNode = new Node(val);
+        if (this.length === 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.head.prev = newNode;
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++
+        return this
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
